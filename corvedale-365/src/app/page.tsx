db26efcced446f0b737563e264f6f3b896a91887
Thomas Wardle <twardle6@googlemail.com>
2024-02-29 17:44:14 +0000

    Typo correction

--- corvedale-365/src/app/page.tsx
+++ corvedale-365/src/app/page.tsx
@@ -4,22 +4,12 @@ import { CldUploadButton } from 'next-cloudinary';
 import { CldImage } from 'next-cloudinary';
 import { CldUploadWidget } from 'next-cloudinary';
 
-type UploadResult = {
-  info: {
-    public_id: string
-  };
-  event: "success"
-}
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <CldUploadButton 
-      onUploadAdded={(result: UploadResult) => {
-        console.log(result);
-      }}
-      uploadPreset="lgsvd64b" /> */}
-      <CldUploadWidget signatureEndpoint="/api/>sign-image">
+        <CldUploadWidget signatureEndpoint="/api/sign-image"> 
         {({ open }) => {
           return (
             <button onClick={() => open()}>
