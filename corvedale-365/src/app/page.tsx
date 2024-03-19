27cbe928370a3776d85f086da4222924f4cf320f
Thomas Wardle <twardle6@googlemail.com>
2024-03-19 11:31:41 +0000

    Top bar completed

--- corvedale-365/src/app/page.tsx
+++ corvedale-365/src/app/page.tsx
@@ -1,30 +1,24 @@
 "use client";
 
-import { CldUploadButton } from 'next-cloudinary';
-import { CldImage } from 'next-cloudinary';
-import { CldUploadWidget } from 'next-cloudinary';
-
-
+import { CldUploadButton } from "next-cloudinary";
+import { CldImage } from "next-cloudinary";
+import { CldUploadWidget } from "next-cloudinary";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <CldUploadWidget signatureEndpoint="/api/sign-image"> 
-        {({ open }) => {
-          return (
-            <button onClick={() => open()}>
-            Upload an Image
-          </button>
-        );
-      }}
-</CldUploadWidget>
-      <CldImage
-        width="960"
-        height="600"
-        src="cld-sample-2"
-        sizes="100vw"
-        alt="Description of my image"
-      />
-    </main> 
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<CldUploadWidget signatureEndpoint="/api/sign-image">
+				{({ open }) => {
+					return <button onClick={() => open()}>Upload an Image</button>;
+				}}
+			</CldUploadWidget>
+			<CldImage
+				width="960"
+				height="600"
+				src="cld-sample-2"
+				sizes="100vw"
+				alt="Description of my image"
+			/>
+		</main>
+	);
 }
